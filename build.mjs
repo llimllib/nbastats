a00f833a538492a2b079b6718ed09063941f2369
Bill Mill <bill@billmill.org>
2021-11-03 20:25:40 -0400

    enable sourcemap

--- build.mjs
+++ build.mjs
@@ -41,6 +41,7 @@ esbuild.build({
   entryPoints: ['viewer.js'],
   bundle: true,
   outfile: 'dist/viewer_duckdb.js',
+  sourcemap: true,
   platform: 'browser',
   format: 'esm',
   resolveExtensions: ['.ts', '.tsx', '.js', '.mjs', '.jsx', '.css', '.wasm'],
