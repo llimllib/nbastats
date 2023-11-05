9751faaf111c0bc2fd524b81171071593d445e4f
Bill Mill <bill@billmill.org>
2023-11-05 07:55:27 -0500

    update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,7 @@
+/* eslint-env node */
 module.exports = {
-  globals: {
-    "d3": true,
-    "process": "readonly"
-  },
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  rules: {
-    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
 };
