ce2e4f87e542193a3d3e2a6b3879466d34cd82e1
Bill Mill <bill@billmill.org>
2023-12-30 21:45:14 -0500

    ci: convince eslint to cool it a bit

--- teamdiamond/.eslintrc.cjs
+++ teamdiamond/.eslintrc.cjs
@@ -1,15 +1,11 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   root: true,
   rules: {
-    'no-unused-vars': [
-      'error',
-      {
-        args: 'none',
-      },
-    ],
-  }
+    "no-unused-vars": ["error", { args: "none" }],
+    "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
+  },
 };
